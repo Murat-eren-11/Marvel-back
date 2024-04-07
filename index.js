d47a771c1e9b9714eab430806a1a1eae54eccd76
Murat-eren-11 <e.murat75@gmail.com>
2024-04-07 12:51:50 +0200

    mongodb added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
 );
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/");
+mongoose.connect(process.env.MONGODB_URI);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "C'est l'API Marvel de Murat !" });
